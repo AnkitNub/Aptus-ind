02c94fe7b679c8b2f5ce8993d8e245622840ff06
AnkitNub <add1ct3dsoul@gmail.com>
2025-03-24 12:35:45 +0530

    Alignement fixed, favicon added, country code option added to contact form

--- script.js
+++ script.js
@@ -149,3 +149,9 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+const phoneInputField = document.querySelector('#phone');
+const phoneInput = window.intlTelInput(phoneInputField, {
+  preferredCountries: ['us', 'co', 'in', 'de'],
+  utilsScript:
+    'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js',
+});
